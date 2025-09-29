95a018cf666efc8fed17dd9f1964a4af481c1134
Alvaro Perez <aperezm15@unicartagena.edu.co>
2025-09-29 13:53:53 -0500

    Creacion de Excepciones

---
+++ Domain/src/main/java/com/mitzuke/domain/exceptions/ReservaInvalidaExcepton.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mitzuke.domain.exceptions;
+
+/**
+ *
+ * @author Mitzuke
+ */
+public class ReservaInvalidaExcepton extends RuntimeException{
+    public ReservaInvalidaExcepton(String message) {
+        super(message);
+    }
+}
