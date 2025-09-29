e541d514469ec4a6aa076489933c45a33c693e5d
Alvaro Perez <aperezm15@unicartagena.edu.co>
2025-09-29 13:48:57 -0500

    Creacion de Enums

---
+++ Domain/src/main/java/com/mitzuke/domain/enums/ClaseAsiento.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mitzuke.domain.enums;
+
+/**
+ *
+ * @author Mitzuke
+ */
+public enum ClaseAsiento {
+    TURISTA,
+    PRIMERA_CLASE
+}
---
+++ Domain/src/main/java/com/mitzuke/domain/enums/RegimenHospedaje.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mitzuke.domain.enums;
+
+/**
+ *
+ * @author Mitzuke
+ */
+public enum RegimenHospedaje {
+    MEDIA_PENSION,
+    PENSION_COMPLETA
+    
+}
