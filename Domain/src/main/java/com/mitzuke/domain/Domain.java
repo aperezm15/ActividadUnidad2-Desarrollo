39ab14a038fa140894b962fc6d8514b8dddb8afa
Alvaro Perez <aperezm15@unicartagena.edu.co>
2025-09-29 13:45:30 -0500

    Creacion de Proyecto

---
+++ Domain/src/main/java/com/mitzuke/domain/Domain.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mitzuke.domain;
+
+/**
+ *
+ * @author Mitzuke
+ */
+public class Domain {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
