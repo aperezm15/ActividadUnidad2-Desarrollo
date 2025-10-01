f48082e42b06ff2ed2f674b7de7bedb9a7974c6b
Alvaro Perez <aperezm15@unicartagena.edu.co>
2025-09-30 19:26:25 -0500

    Creacion de ReservaFactory
    Para reservar vuelo y hospedaje

---
+++ Domain/src/main/java/com/mitzuke/domain/services/ReservaFactory.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mitzuke.domain.services;
+
+import com.mitzuke.domain.model.*;
+import com.mitzuke.domain.enums.*;
+import com.mitzuke.domain.exceptions.ReservaInvalidaException;
+import com.mitzuke.domain.valueobjects.*;
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ *
+ * @author Mitzuke
+ */
+public class ReservaFactory {
+    private static final AtomicLong ID_GENERATOR = new AtomicLong(0);
+    
+    
+    public ReservaVuelo crearReservaVuelo(CodigoTuristaId turistaId,
+            NumeroVueloId vueloId, ClaseAsiento claseAsiento,
+            FechaViaje fechaVuelo, int numeroAsientos) {
+                if (fechaVuelo.valor().isBefore(java.time.LocalDateTime.now().plusHours(24))) {
+                    throw new ReservaInvalidaException("Los vuelos deben reservarse con al menos 24 horas de antelacion");
+                }
+                
+                Long nuevoId = ID_GENERATOR.incrementAndGet();
+                
+                return new ReservaVuelo(nuevoId, turistaId, vueloId, claseAsiento, fechaVuelo, numeroAsientos);
+            }
+}
